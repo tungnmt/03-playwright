27cc9e9373e6c75afe1af4cd3d0fe4fe14e3bb8d
tungnmt561@gmail.com <tungnmt561@gmail.com>
2026-07-15 22:30:59 +0700

    commit note

--- lesson-02/main.js
+++ lesson-02/main.js
@@ -7,3 +7,14 @@ const myname = "Nguyễn Minh Tùng";
 console.log(myname);
 console.log(myname);
 console.log(myname);
+const a = 10;
+const b = 20;
+console.log(a > b);
+
+// So sánh == và ===
+// So sánh khác !== 
+
+// *Toán tử logic Và: && Hoặc ||
+//Toán tử 1 ngôi, Ví dụ: Prefix: toán tử nằm trước +- trước, trả giá trị sau b = ++a (let a=10 => in ra a =11, b=11)
+
+// Postfix: toán tủ nằm sau : trả về giá trị trước rồi mới tăng b= a++ (let a =10 => in ra a=11, b=10)
