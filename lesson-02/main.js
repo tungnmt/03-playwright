02aa1569e3cf7195785cb3697c2bf0a7a1093167
tungnmt561@gmail.com <tungnmt561@gmail.com>
2026-07-15 11:58:53 +0700

    commit javascript

---
+++ lesson-02/main.js
@@ -0,0 +1,9 @@
+console.log("Hello, World!");
+let myname1 = "Nguyễn Minh Tùng";
+console.log(myname1);
+
+// Hằng-Không thay đổi
+const myname = "Nguyễn Minh Tùng";
+console.log(myname);
+console.log(myname);
+console.log(myname);
