9eba33f467396e8f80a8241955a44bb76e87ddf7
tungnmt561@gmail.com <tungnmt561@gmail.com>
2026-07-20 22:21:39 +0700

    feat: test

--- lesson/lesson-04.js
+++ lesson/lesson-04.js
@@ -4,3 +4,18 @@ if (hour > 10){
 } else{
 console.log("Giá trị nhỏ hơn 10")
 }
+
+// hàm if
+// const diem = 6;
+// if (diem >= 0 && diem <=5){
+//     console.log("học sinh trung bình")
+// }else if(diem >5 && diem <8 ) {
+//     console.log("học sinh khá")
+// }else if(diem >=8){
+//     console.log("học sinh giỏi")
+// }
+
+
+for (let i = 0; i < 10; i++){
+    console.log("Nguyễn Minh Tùng " + i)
+}
